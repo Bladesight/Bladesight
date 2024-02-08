d8eba4d32b7e18fb0a1d1db0c9853b0e07ee7d2a
Dawie Diamond <dawie@invokeanalytics.co.za>
2024-02-08 05:11:16 +0200

    Final pass with unit tests for dataset_handler

--- tests/test_download_datasets.py
+++ tests/test_download_datasets.py
@@ -3,6 +3,7 @@ from bladesight.dataset_handler import (
     download_dataset_from_bladesight_data,
     BladesightDatasetDirectory
 )
+import bladesight.dataset_handler
 import os
 import pytest
 
@@ -14,7 +15,7 @@ def test_download_dataset_from_bladesight_data(tmp_path):
     download_dataset_from_bladesight_data("bladesight-datasets/intro_to_btt/intro_to_btt_ch05")
     assert get_local_datasets() == ["data/intro_to_btt/intro_to_btt_ch03", "data/intro_to_btt/intro_to_btt_ch05"]
 
-def test_Datasets(tmp_path):
+def test_Datasets(tmp_path, monkeypatch):
     """This function will test whether we can return 
     datasets that have been downloaded. 
     """
@@ -24,3 +25,18 @@ def test_Datasets(tmp_path):
     df = ds["table/du_toit_2017_test_1_opr_zero_crossings"]
     assert df.shape == (791, 1)
     assert df.columns == ["time"]
+    def mock_download_dataset_from_bladesight_data(*args, **kwargs):
+        raise ValueError("Mocked error")
+    monkeypatch.setattr(
+        bladesight.dataset_handler, 
+        "download_dataset_from_bladesight_data", 
+        mock_download_dataset_from_bladesight_data
+    )
+    Datasets = BladesightDatasetDirectory()
+    ds = Datasets["data/intro_to_btt/intro_to_btt_ch03"]
+    df = ds["table/du_toit_2017_test_1_opr_zero_crossings"]
+    assert df.shape == (791, 1)
+    with pytest.raises(ValueError):
+        Datasets["data/intro_to_btt/intro_to_btt_ch05"]
+    with pytest.raises(KeyError):
+        Datasets["error/intro_to_btt/intro_to_btt_ch05"]
