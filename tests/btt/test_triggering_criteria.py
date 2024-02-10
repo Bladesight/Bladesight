3d393f9447ca0ade9ded5346ce14dd7cf9f14ab2
Dawie Diamond <dawie@invokeanalytics.co.za>
2024-02-10 09:44:27 +0200

    Started tests for downwards slope

--- tests/btt/test_triggering_criteria.py
+++ tests/btt/test_triggering_criteria.py
@@ -12,6 +12,18 @@ def arr_simple_ramp_up():
     """
     return np.arange(10), np.arange(10)
 
+@pytest.fixture
+def arr_simple_ramp_down():
+    """ Simplest ramp down signal.
+        (n, n) -> (n, n) for n in [0, 10)
+
+    Returns:
+        np.ndarray: A simple ramp up signal.
+        np.ndarray: The time array.
+    """
+    return np.arange(10), np.array(list(np.arange(10))[::-1])
+
+
 def test_threshold_crossing_interp(arr_simple_ramp_up):
     arr_t, arr_s = arr_simple_ramp_up
     arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 5)
@@ -36,6 +48,27 @@ def test_threshold_crossing_interp_simple_first_sample(arr_simple_ramp_up):
     assert arr_toa[0] == 9.0
     arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 9.1)
     assert len(arr_toa) == 0
+    # Test that triggering on the falling edge does not work
+    arr_toa = triggering_criteria.threshold_crossing_interp(
+        arr_t, 
+        arr_s, 
+        5, 
+        trigger_on_rising_edge=False
+    )
+    assert len(arr_toa) == 0
+
+def test_threshold_crossing_interp_falling(arr_simple_ramp_down):
+    arr_t, arr_s = arr_simple_ramp_down
+    arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 5, trigger_on_rising_edge=False)
+    assert len(arr_toa) == 1
+    assert np.allclose(arr_toa[0], 4.0)
+    arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 4.9, trigger_on_rising_edge=False)
+    assert len(arr_toa) == 1
+    assert np.allclose(arr_toa[0],4.1)
+    arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 5.1, trigger_on_rising_edge=False)
+    assert len(arr_toa) == 1
+    assert np.allclose(arr_toa[0], 3.9)
+
 
 
 # Test threshold_crossing_interp todo:
