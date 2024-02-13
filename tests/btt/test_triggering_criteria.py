a89a40baf9267e7c57cbdd9f3f019095c0c26bc6
Dawie Diamond <dawie@invokeanalytics.co.za>
2024-02-13 20:13:02 +0200

    Confirm that simple triggering threshold triggers multiple times

--- tests/btt/test_triggering_criteria.py
+++ tests/btt/test_triggering_criteria.py
@@ -90,6 +90,30 @@ def test_threshold_crossing_interp_simple_first_sample_falling(arr_simple_ramp_d
     )
     assert len(arr_toa) == 0
 
+@pytest.fixture
+def arr_zig_zag():
+    """ A zig zag signal that is supposed to trigger multiple times
+    when using the simple threshold.
+
+    Returns:
+        np.ndarray: A zig zag signal.
+        np.ndarray: The time array.
+    """
+    return np.arange(10), np.array([0, 5, 0, 5, 0, 5, 0, 5, 0, 5])
+
+def test_threshold_crossing_interp_zig_zag_rising(arr_zig_zag):
+    arr_t, arr_s = arr_zig_zag
+    arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 2.5)
+    assert len(arr_toa) == 5
+    assert np.allclose(arr_toa, [0.5, 2.5, 4.5, 6.5, 8.5])
+
+def test_threshold_crossing_interp_zig_zag_falling(arr_zig_zag):
+    arr_t, arr_s = arr_zig_zag
+    arr_toa = triggering_criteria.threshold_crossing_interp(arr_t, arr_s, 2.5, trigger_on_rising_edge=False)
+    assert len(arr_toa) == 4
+    assert np.allclose(arr_toa, [1.5, 3.5, 5.5, 7.5])
+
+
 
 # Test threshold_crossing_interp todo:
 # - Test with n_est, ensure exception is raised if 
