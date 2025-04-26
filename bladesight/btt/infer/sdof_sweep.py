ff7c335a014930bcf6df4b2c6dacca25a7f2ac22
Justin Smith <66smithjustin@gmail.com>
2025-04-26 14:40:09 +0200

    Added perform_sdof_sweep_fit to sdof_sweep.py.
    - Note the paper by Zhi uses Omega_n to denote the speed at resonance. To date, I have used this incorrectly as the natural frequency. I updated this throughout sdof_sweep.py
    - Added analogous function to sdof_sweep.py to perform the fit and return the optimal parameters. This function is analogous to perform_SDoF_fitin sdof.py
    - Updated some function's names to be consistent with sdof.py

--- bladesight/btt/infer/sdof_sweep.py
+++ bladesight/btt/infer/sdof_sweep.py
@@ -13,15 +13,18 @@ https://docs.bladesight.com/tutorials/datasets/diamond_et_al_2024_vary_ramp_rate
 import numpy as np
 import pandas as pd
 from scipy.optimize import differential_evolution
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 
 from .sdof import get_phi
 
 # ------------------ Frequency Sweep Parameter Method ------------------ #
-def get_eta(EO: int, Q: float, a: float, 
-            #f_n: float,
-            Omega_n:float) -> float:
+def get_eta(
+            EO: int, 
+            Q: float, a: float, 
+            # f_n: float,
+            speed_at_resonance:float
+            ) -> float:
     """
     Compute the non-dimensional frequency sweep parameter η.
 
@@ -36,10 +39,11 @@ def get_eta(EO: int, Q: float, a: float,
         Quality factor (dimensionless).
     a : float
         Frequency sweep rate (acceleration of rotating speed).
-    # f_n : float
-    #     Natural frequency (in Hz or consistent units).
-    Omega_n : float
-        The rotating speed at resonance (in radians/second).
+    f_n : float
+        Natural frequency (in Hz or consistent units).
+    speed_at_resonance : float
+        The rotating speed at resonance (in radians/second). 
+        This is denoted by Ωₙ in Reference [1] but this notation is not used here to avoid confusion with the omega_n term in sdof.py that denotes the blade vibration natural frequency.
 
     Returns
     -------
@@ -51,8 +55,8 @@ def get_eta(EO: int, Q: float, a: float,
     [1] F. Zhi et al., "Error Revising of Blade Tip-Timing Parameter Identification Caused by Frequency Sweep Rate",
         Measurement, vol. 201, p. 111681, Sep. 2022, doi: 10.1016/j.measurement.2022.111681.
     """
-    # return EO*((Q**2) * a)/(60*(f_n**2))
-    return ((Q**2) * a)/(60*EO*(Omega_n**2)) # Equation 11 in Reference [1], NOT using f_n as its just an extra parameter to pass in unnecessarily
+    # return EO*((Q**2) * a)/(60*(f_n**2)) # Equation 11 in Reference [1], NOT using Omega_n as its confusing with existing bladesight notation in sdof.py
+    return ((Q**2) * a)/(60*EO*(speed_at_resonance**2)) # Equation 11 in Reference [1], NOT using f_n as its just an extra parameter to pass in unnecessarily
 
 def get_A(eta: float) -> float:
     """
@@ -191,7 +195,7 @@ def get_frequency_sweep_rate_a(speed_at_start: float, speed_at_end: float, time_
     """
     return (speed_at_end - speed_at_start) / (time_at_end - time_at_start)
 
-def predict_sdof_samples_sweep(
+def predict_SDoF_sweep_samples(
     EO : int,
     theta_sensor : float,
     phi_0 : float,
@@ -201,7 +205,7 @@ def predict_sdof_samples_sweep(
     # f_n: float,
     A_max: float,
     C: float,
-    Omega_n: float, # Moved Omega_n up here so it can be optimised
+    speed_at_resonance: float, # Moved Omega_n up here so it can be optimised
 
 
     Omega_arr: np.ndarray,
@@ -252,8 +256,9 @@ def predict_sdof_samples_sweep(
         Resonant maximum amplitude of the blade vibration.
     C : float
         Constant vibration offset term used when calculating the predicted tip deflections.
-    Omega_n : float
-        The rotating speed at resonance (in radians/second).
+    speed_at_resonance : float
+        The rotating speed at resonance (in radians/second). 
+        This is denoted by Ωₙ in Reference [1] but this notation is not used here to avoid confusion with the omega_n term in sdof.py that denotes the blade vibration natural frequency.
     Omega_arr : np.ndarray
         Array of excitation frequencies (in radians/second).
     time_arr : np.ndarray
@@ -294,9 +299,11 @@ def predict_sdof_samples_sweep(
         print(f"Frequency sweep rate, a: {a} (Rad/s)/s = {a*60/(2*np.pi)} (RPM/s)")
 
     eta = get_eta(
-                EO = EO, Q = Q, a = a, 
+                EO = EO, 
+                Q = Q, 
+                a = a, 
                 #   f_n = f_n,
-                Omega_n = Omega_n
+                speed_at_resonance = speed_at_resonance
                 )
     
     A = get_A(eta=eta)
@@ -320,11 +327,11 @@ def predict_sdof_samples_sweep(
                 # (Omega_arr/((f/Q + 1)*Omega_n))
                 (
                     1 - (
-                        (Omega_arr/((f/Q + 1)*Omega_n))**2
+                        (Omega_arr/((f/Q + 1)*speed_at_resonance))**2
                         )
                 )
                 /
-                (Omega_arr/((f/Q + 1)*Omega_n))
+                (Omega_arr/((f/Q + 1)*speed_at_resonance))
                 )
 
     numerator_term1 = A * A_max
@@ -333,14 +340,14 @@ def predict_sdof_samples_sweep(
     # denominator_term1 = Omega_arr/(f/Q + 1)*Omega_n #Option 1
     #                                                 # Not sure if its .../((f/Q + 1)*speed_n) or if its .../(f/Q + 1)*speed_n
     denominator_term1 = Omega_arr/( # Option 2
-                                    (f/Q + 1)*Omega_n) # Not sure if its .../((f/Q + 1)*speed_n) or if its .../(f/Q + 1)*speed_n
+                                    (f/Q + 1)*speed_at_resonance) # Not sure if its .../((f/Q + 1)*speed_n) or if its .../(f/Q + 1)*speed_n
     denominator_term2 = (v**2 + 1)
 
     predicted_tip_deflections = (numerator_term1/denominator_term1) * (numerator_term2/denominator_term2) + C
     
     return predicted_tip_deflections
 
-def SDoF_loss_multiple_probes_sweep(
+def SDoF_sweep_loss_multiple_probes(
     model_params : np.ndarray,
     tip_deflections_set : List[np.ndarray],
     EO : int, 
@@ -373,8 +380,9 @@ def SDoF_loss_multiple_probes_sweep(
             Resonant maximum amplitude of the blade vibration.
         - C : float
             Constant vibration offset term used when calculating the predicted tip deflections.
-        - Omega_n : float
-            The rotating speed at resonance (in radians/second).
+        - speed_at_resonance : float
+            The rotating speed at resonance (in radians/second). 
+            This is denoted by Ωₙ in Reference [1] but this notation is not used here to avoid confusion with the omega_n term in sdof.py that denotes the blade vibration natural frequency.
     tip_deflections_set : list of np.ndarray
         Measured tip deflections for each probe.
     EO : int
@@ -409,7 +417,7 @@ def SDoF_loss_multiple_probes_sweep(
     #                             speed_at_start = Omega_arr[0], speed_at_end = Omega_arr[-1],
     #                             time_at_start = time_arr[0], time_at_end = time_arr[-1]
     #                             )
-    phi_0, Q, A_max, C, Omega_n = model_params
+    phi_0, Q, A_max, C, speed_at_resonance = model_params
 
     # zeta = np.exp(ln_zeta)
 
@@ -417,7 +425,7 @@ def SDoF_loss_multiple_probes_sweep(
     error = 0
     for i_probe, arr_tip_deflections in enumerate(tip_deflections_set):    
         theta_sensor = theta_sensor_set[i_probe]
-        predicted_tip_deflections = predict_sdof_samples_sweep(
+        predicted_tip_deflections = predict_SDoF_sweep_samples(
             # omega_n, zeta, delta_st, EO, theta_sensor, phi_0, arr_omega
             EO = EO,
             theta_sensor = theta_sensor,
@@ -430,22 +438,10 @@ def SDoF_loss_multiple_probes_sweep(
             Omega_arr = Omega_arr,
             time_arr = time_arr,
 
-            Omega_n = Omega_n, # now optimising for Omega_n
+            speed_at_resonance = speed_at_resonance, # now optimising for speed_at_resonance so its included in model_params
 
             # verbose = verbose
             )   
-
-        # z_median = correction_factors[i_probe*2]
-        # z_max = correction_factors[i_probe*2+1]
-        # arr_tip_deflection_corrections = predict_sdof_samples_sweep(
-        #     arr_omega, z_median, z_max
-        # )
-        # arr_tip_deflections_corrected = (
-        #     arr_tip_deflections
-        #     + arr_tip_deflection_corrections
-        # )
-
-        # arr_tip_deflection = predicted_tip_deflections
         error += np.sum(
             np.abs(arr_tip_deflections)**amplitude_scaling_factor
             *(
@@ -456,94 +452,175 @@ def SDoF_loss_multiple_probes_sweep(
     return error
 
 
-# def perform_SDoF_fit(
-#     df_blade : pd.DataFrame,
-#     n_start : int,
-#     n_end : int,
-#     EOs : List[int] = np.arange(1, 50),
-#     # delta_st_max : int = 10,
-#     verbose : bool = False,
-#     probe_column_label_suffix : str = "_filt"
-# ) -> Dict[str, float]:
-#     """This function receives a blade tip deflection DataFrame, and returns 
-#     the SDoF fit model parameters after fitting.
+def perform_SDoF_sweep_fit(
+    df_blade : pd.DataFrame,
+    n_start : int,
+    n_end : int,
+    EOs : Optional[List[int]] = np.arange(1, 20),
+    # omega_n_bounds : Optional[List[float]] = [None, None],
+    # zeta_bounds : Optional[List[float]] = [0.0001, 0.3],
+    # delta_st_bounds : Optional[List[float]] = [0, 10],
+    # phi_0_bounds : Optional[List[float]] = [0, 2*np.pi],
 
-#     Args:
-#         df_blade (pd.DataFrame): The blade tip deflection DataFrame.
-#         n_start (int): The starting revolution number of the resonance 
-#             you want to fit.
-#         n_end (int): The ending revolution number of the resonance 
-#             you want to fit.
-#         EOs (List[int], optional): The list of EOs to search for. Defaults 
-#             to np.arange(1, 20).
-#         delta_st_max (int, optional): The maximum static deflection within our optimization 
-#             bounds. Defaults to 10.
-#         verbose (bool, optional): Whether to print the progress. Defaults to False.
+    phi_0_bounds : Optional[List[float]] = [0, 2*np.pi],
+    Q_bounds : Optional[List[float]] = [1, 1000],
+    A_max_bounds : Optional[List[float]] = [-1000, 1000],
+    C_bounds : Optional[List[float]] = [-1000, 1000],
+    speed_at_resonance_bounds : Optional[List[float]] = [None, None],
 
-#     Returns:
-#         Dict[str, float]: The fitted model parameters.
-#     """
-#     df_resonance_window = df_blade.query(f"n >= {n_start} and n <= {n_end}")
-#     measured_tip_deflection_signals = [
-#         col 
-#         for col in df_resonance_window
-#         if col.endswith(probe_column_label_suffix)
-#     ]
-#     PROBE_COUNT = len(measured_tip_deflection_signals)
-#     eo_solutions = []
-#     for EO in EOs:
-#         if verbose:
-#             print("NOW SOLVING FOR EO = ", EO, " of ", EOs)
-#         omega_n_min = df_resonance_window["Omega"].min() * EO
-#         omega_n_max = df_resonance_window["Omega"].max() * EO
-#         ln_zeta_min = np.log(0.0001)
-#         ln_zeta_max = np.log(0.3)
-#         delta_st_min = 0
-#         phi_0_min = 0
-#         phi_0_max = 2*np.pi
-#         bounds = [
-#             (omega_n_min, omega_n_max),
-#             (ln_zeta_min, ln_zeta_max),
-#             (delta_st_min, delta_st_max),
-#             (phi_0_min, phi_0_max),
-#         ]
-#         tip_deflections_set = []
-#         theta_sensor_set = []
-#         for i_probe in range(PROBE_COUNT):
-#             z_max = df_resonance_window[f"x_p{i_probe+1}"+probe_column_label_suffix].abs().max()
-#             z_min = -z_max
-#             bounds.extend(
-#                 [
-#                     (z_min, z_max),
-#                     (z_min, z_max)
-#                 ]
-#             )
-#             tip_deflections_set.append(
-#                 df_resonance_window[f"x_p{i_probe+1}"+probe_column_label_suffix].values
-#             )
-#             theta_sensor_set.append(
-#                 df_resonance_window[f"AoA_p{i_probe+1}"].median()
-#             )
-#         multiple_probes_solution = differential_evolution(
-#             func = SDoF_loss_multiple_probes,
-#             bounds=bounds,
-#             args=(
-#                 tip_deflections_set,
-#                 df_resonance_window['Omega'].values,
-#                 EO,
-#                 theta_sensor_set,
-#                 2
-#             ),
-#             seed=42
-#         )
-#         eo_solutions.append(multiple_probes_solution)
-#     best_EO_arg = np.argmin([solution.fun for solution in eo_solutions])
-#     best_EO = EOs[best_EO_arg]
-#     best_solution = eo_solutions[best_EO_arg]
-#     return {
-#         "omega_n" : best_solution.x[0] / (2*np.pi),
-#         "zeta" : np.exp(best_solution.x[1]),
-#         "delta_st" : best_solution.x[2],
-#         "phi_0" : best_solution.x[3],
-#         "EO" : best_EO,
-#     }
+
+
+    signal_suffix : Optional[str] = "_filt" ,
+    amplitude_scaling_factor : float = 1,
+    differential_evolution_optimiser_kwargs: Optional[dict] = {'seed': 42},
+    verbose : bool = False
+) -> Dict[str, float]:
+    """
+    Fit an SDoF model to blade tip deflection data over a specified revolution range.
+
+    Parameters
+    ----------
+    df_blade : pd.DataFrame
+        The blade tip deflection DataFrame.
+    n_start : int
+        The starting revolution number of the resonance to be used in the fit.
+    n_end : int
+        The ending revolution number of the resonance to be used in the fit.
+    EOs : List[int], optional
+        Range of engine orders to consider, defaults to np.arange(1, 20).
+
+
+
+    # omega_n_bounds : List[float], optional
+    #     [min, max] bounds for resonant speed Ωₙ (radians/second). If None, set per EO as
+    #     (min(df_blade['Omega'])*EO, max(df_blade['Omega'])*EO).
+    # zeta_bounds : list of float, optional
+    #     [min, max] bounds for damping ratio ζ, default to: [0.0001, 0.3].
+    # delta_st_bounds : list of float, optional
+    #     [min, max] bounds for static deflection δₛₜ, , defaults to [0, 10].
+    # phi_0_bounds : list of float, optional
+    #     [min, max] bounds for phase offset φ₀ in radians, defaults to [0, 2π].
+
+    
+    phi_0_bounds : list of float, optional
+        [min, max] bounds for phase offset φ₀ in radians, defaults to [0, 2π].
+    Q_bounds : list of float, optional
+        [min, max] bounds for quality factor Q, defaults to [1, 1000].
+    A_max_bounds : list of float, optional
+        [min, max] bounds for resonant maximum amplitude A_max, defaults to [-1000, 1000].
+    C_bounds : list of float, optional
+        [min, max] bounds for constant vibration offset C, defaults to [-1000, 1000].
+    speed_at_resonance_bounds : list of float, optional
+        [min, max] bounds for speed at resonance (Ωₙ), defaults to [None, None].
+    
+    signal_suffix : str, optional
+        Suffix appended to deflection column names, defaults to "_filt".
+
+        
+    # amplitude_scaling_factor : float, optional
+    #     Scaling factor for amplitude weighting, defaults to 1.
+
+
+    differential_evolution_optimiser_kwargs : dict, optional
+        Additional arguments for the differential evolution optimizer, defaults to {'seed': 42}.
+        If you want to use the default settings, pass an empty dictionary.
+    verbose : bool, optional
+        If True, print progress updates, defaults to False.
+    Returns:
+        Dict[str, float]: The fitted model parameters.
+    Returns
+    -------
+    Dict[str, Union[float, int]]
+        Fitted parameters, including:
+        # - "omega_n": Natural frequency (in Hz).
+        # - "zeta": Damping ratio.
+        # - "delta_st": Static deflection.
+        # - "phi_0": Phase offset in radians.
+        # - "EO": Engine order with minimum error.
+
+        - "phi_0": Initial phase of the excitation force (in radians).
+        - "Q": Quality factor.
+        - "A_max": Resonant maximum amplitude.
+        - "C": Constant vibration offset.
+        - "speed_at_resonance": Speed at resonance (in radians/second).
+        - "EO": Engine order with minimum error.
+    """
+    df_resonance_window = df_blade.query(f"n >= {n_start} and n <= {n_end}")
+    measured_tip_deflection_signals = [
+        col 
+        for col in df_resonance_window
+        if col.endswith("_filt")
+    ]
+    PROBE_COUNT = len(measured_tip_deflection_signals)
+    EO_solutions = []
+    for EO in EOs:
+        if verbose:
+            print("NOW SOLVING FOR EO = ", EO, " of ", EOs)
+        
+        if speed_at_resonance_bounds.__contains__(None):
+            speed_at_resonance_bounds[0] = df_resonance_window["Omega"].min()
+            speed_at_resonance_bounds[1] = df_resonance_window["Omega"].max()
+        if speed_at_resonance_bounds.__contains__(None) == False:
+            speed_at_resonance_bounds[0] = speed_at_resonance_bounds[0]
+            speed_at_resonance_bounds[1] = speed_at_resonance_bounds[1]
+
+        bounds = [
+            # (omega_n_bounds[0], omega_n_bounds[1]),
+            # (zeta_bounds[0], zeta_bounds[1]),
+            # (delta_st_bounds[0], delta_st_bounds[1]),
+            # (phi_0_bounds[0], phi_0_bounds[1]),
+            (phi_0_bounds[0], phi_0_bounds[1]),
+            (Q_bounds[0], Q_bounds[1]),
+            (A_max_bounds[0], A_max_bounds[1]),
+            (C_bounds[0], C_bounds[1]),
+            (speed_at_resonance_bounds[0], speed_at_resonance_bounds[1]),
+        ]
+        tip_deflections_set = []
+        theta_sensor_set = []
+        for i_probe in range(PROBE_COUNT):
+            z_max = df_resonance_window[f"x_p{i_probe+1}"+signal_suffix].abs().max()
+            z_min = -z_max
+            bounds.extend(
+                [
+                    (z_min, z_max),
+                    (z_min, z_max)
+                ]
+            )
+            tip_deflections_set.append(
+                df_resonance_window[f"x_p{i_probe+1}"+signal_suffix].values
+            )
+            theta_sensor_set.append(
+                df_resonance_window[f"AoA_p{i_probe+1}"].median()
+            )
+        multiple_probes_solution = differential_evolution(
+            func = SDoF_sweep_loss_multiple_probes,
+            bounds=bounds,
+            args=(
+                tip_deflections_set,
+                EO,
+                theta_sensor_set,
+                df_resonance_window['Omega'].values,
+                df_resonance_window[f'ToA_p{i_probe+1}'].values,
+                amplitude_scaling_factor,
+            ),
+            **differential_evolution_optimiser_kwargs
+        )
+        EO_solutions.append(multiple_probes_solution)
+    
+    # Select the best EO
+    best_EO_arg = np.argmin([solution.fun for solution in EO_solutions])
+    best_EO = EOs[best_EO_arg]
+    best_solution = EO_solutions[best_EO_arg]
+    return {
+        # "omega_n" : best_solution.x[0] / (2*np.pi),
+        # "zeta" : np.exp(best_solution.x[1]),
+        # "delta_st" : best_solution.x[2],
+        # "phi_0" : best_solution.x[3],
+        # "EO" : best_EO,
+        "phi_0" : best_solution.x[0],
+        "Q" : best_solution.x[1],
+        "A_max" : best_solution.x[2],
+        "C" : best_solution.x[3],
+        "speed_at_resonance" : best_solution.x[4],
+        "EO" : best_EO,
+    }
